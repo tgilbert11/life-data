9a9f0b7e2316cddc13296941ae90d79777149d69
Taylor Gilbert <taylorg353@gmail.com>
2015-10-14 09:46:20 -0700

    .gitignore is on

---
+++ life_data/WeekViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WeekViewController.swift
+//  life_data
+//
+//  Created by Taylor H. Gilbert on 10/6/15.
+//  Copyright © 2015 Taylor H. Gilbert. All rights reserved.
+//
+
+import Foundation
